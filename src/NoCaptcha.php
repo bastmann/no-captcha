045156235a6c23efc07afa6f6007164940c214f7
Kristian Drucker <oxkipo@googlemail.com>
2016-08-17 14:41:40 +0200

    Update NoCaptcha.php

--- src/NoCaptcha.php
+++ src/NoCaptcha.php
@@ -45,7 +45,7 @@ class NoCaptcha
         $attributes['data-sitekey'] = $this->sitekey;
 
         $html = '<script src="'.$this->getJsLink($lang).'" async defer></script>'."\n";
-        $html .= '<div class="g-recaptcha"'.$this->buildAttributes($attributes).'></div>';
+        $html .= '<div v-model="no-captcha" class="g-recaptcha"'.$this->buildAttributes($attributes).'></div>';
 
         return $html;
     }
